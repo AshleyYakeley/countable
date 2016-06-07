1a3df6cfe9303de0447fd464abc2c663e3ddf3b2
Ashley Yakeley <ashley@semantic.org>
2016-06-06 22:40:31 -0700

    test: use cabal

--- test/Count.hs
+++ test/Count.hs
@@ -6,31 +6,31 @@ module Main where
     import Data.Word;
     import Control.Monad;
     import Prelude;
-    
+
     type Property a = a -> Bool;
-    
+
     testProperty :: (Show a) => String -> Property a -> a -> IO ();
     testProperty s prop a = do
     {
         putStrLn (s ++ "(" ++ (show a) ++ "): " ++ (if prop a then "PASS" else "FAIL"));
     };
-    
+
     maybeNextDifferent :: (Countable a) => Property a;
     maybeNextDifferent a = countMaybeNext (Just a) /= Just a;
-    
+
     prevDifferent :: (Countable a) => Property a;
     prevDifferent a = countPrevious a /= Just a;
-    
+
     maybeNextPrev :: (Countable a) => Property a;
     maybeNextPrev a = countMaybeNext (countPrevious a) == Just a;
-    
+
     prevMaybeNext :: (Countable a) => Property (Maybe a);
     prevMaybeNext ma = case countMaybeNext ma of
     {
         Just a' -> countPrevious a' == ma;
         Nothing -> True;
     };
-    
+
     testAllCountable' :: (Show a,Countable a) => a -> IO ();
     testAllCountable' a = do
     {
@@ -40,14 +40,14 @@ module Main where
         testProperty "maybeNextPrev" maybeNextPrev a;
         testProperty "prevMaybeNext" prevMaybeNext (Just a);
     };
-    
+
     testAllCountable :: (Show a,Countable a) => a -> IO ();
     testAllCountable a = do
     {
         testAllCountable' a;
         testProperty "findInNext" (findInNext 1000) a;
     };
-    
+
     testCountableValue :: (Show a, Countable a) => a -> IO ();
     testCountableValue a = let
     {
@@ -57,29 +57,29 @@ module Main where
     {
         putStrLn (show (ma' == Just a,a,ma1,ma'));
     };
-    
+
     nextIsMaybeNext :: (InfiniteCountable a) => Property (Maybe a);
     nextIsMaybeNext ma = countMaybeNext ma == Just (countNext ma);
-    
+
     testAllInfiniteCountable :: (Show a,InfiniteCountable a) => a -> IO ();
     testAllInfiniteCountable a = do
     {
         testAllCountable a;
         testProperty "nextIsMaybeNext" nextIsMaybeNext (Just a);
     };
-    
+
     findInNext :: (Countable a) => Int -> Property a;
     findInNext n a = findInNext' n Nothing where
     {
         findInNext' 0 _ = False;
         findInNext' _ (Just x) | x == a = True;
-        findInNext' n mx = case countMaybeNext mx of
+        findInNext' n' mx = case countMaybeNext mx of
         {
             Nothing -> False;
-            mx' -> findInNext' (n - 1) mx';
+            mx' -> findInNext' (n' - 1) mx';
         };
     };
-    
+
     checkN :: (Show a,Countable a) => Int -> Maybe a -> IO ();
     checkN 0 _ = return ();
     checkN n ma = let
@@ -94,30 +94,30 @@ module Main where
             _ -> checkN (n - 1) ma';
         };
     };
-    
+
     data Three = T1 | T2 | T3 deriving (Eq,Show);
-    
+
     instance Searchable Three where
     {
         search = finiteSearch;
     };
-    
+
     instance Countable Three where
     {
         countPrevious = finiteCountPrevious;
         countMaybeNext = finiteCountMaybeNext;
     };
-    
+
     instance AtLeastOneCountable Three where
     {
         countFirst = T1;
     };
-    
+
     instance Finite Three where
     {
         allValues = [T1,T2,T3];
     };
-    
+
     main :: IO ();
     main = do
     {
@@ -132,8 +132,8 @@ module Main where
         mapM_ testAllCountable ([[],[True,True]] :: [[Bool]]);
         mapM_ testAllInfiniteCountable ([0,1,-1,3,-7] :: [Integer]);
         mapM_ testAllCountable (allValues :: [Three -> Three]);
-        mapM_ testAllCountable (allValues :: [Nothing]);
-        mapM_ testAllCountable ([[] :: [Nothing]]);
+        mapM_ testAllCountable (allValues :: [None]);
+        mapM_ testAllCountable ([[] :: [None]]);
         putStrLn "---";
         checkN 40 (Nothing :: Maybe [Bool]);
         putStrLn "---";
